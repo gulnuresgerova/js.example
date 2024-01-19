ee3c3ab8019a3cac9cf579e392b0586c3ab6761f
gulnuresgerova <akulaalekb@gmail.com>
2024-01-19 13:16:30 -0800

    class

--- tasty/assets/js/favs.js
+++ tasty/assets/js/favs.js
@@ -1,24 +1,58 @@
-let id = new URLSearchParams(window.location.search).get("id");
 
-console.log(id);
-const details = document.querySelector(".details");
 const BASE_URL = "http://localhost:3366/ menu";
 
+const products = document.querySelector(".products");
+const favCount = document.querySelector(".fav-count");
 
-fetch(`${BASE_URL}/${id}`)
-  .then((res) => res.json())
-  .then((el) => {
-    details.innerHTML = `
-    <div class="box">
-    <div class="box-img"><img src="${el.img}" alt=""></div>
-    <div class="box-body">
-        <h5 class="title">${el.title}  <a href="./details.html?id=${element.id}" >Read More</a></div></h5>
-        <p class="desc">${el.desc}</p>
+
+
+let favProduct = getFavaFromLocaleStrogeProduct();
+calculateCount(favProduct.length);
+
+
+
+function drawFavs(data) {
+    products.innerHTML = "";
+    data.forEach((el) => {
+      products.innerHTML += `
+      <div class="box">
+        <div class="box-img"><img src="${el.img}" alt=""></div>
+        <div class="box-body">
+            <h5 class="title">${el.title}</h5>
+            <p class="desc">${el.desc}</p>
+            <i class="${
+              favProduct.some((item) => item.id === el.id)
+                ? "fa-solid fa-heart "
+                : "fa-regular fa-heart"
+            }" onclick=favIcon("${el.id}",this)></i>
+        </div>
+        <div class="price">
+            <h3>$${el.price}</h3>
+        </div>
     </div>
-    <div class="price">
-        <h3>$${el.price}</h3>
-    </div>
-</div>
-    
-    `;
-  });
+        
+      `;
+    });
+  }
+  drawFavs(favProduct)
+
+
+  function favIcon(id,icon){
+favProduct=favProduct.filter((item)=>item.id !== id)
+setFavaFromLocaleStrogeProduct(favProduct)
+calculateCount(favProduct.length)
+
+icon.closest(".box").remove()
+  }
+
+function calculateCount(count){
+    favCount.textContent=count
+}
+
+  function setFavaFromLocaleStrogeProduct(fav){
+    localStorage.setItem("favs", JSON.stringify(fav))
+  }
+  function getFavaFromLocaleStrogeProduct(){
+    return JSON.parse(localStorage.getItem("favs")) || []
+
+  }
--- tasty/assets/js/main.js
+++ tasty/assets/js/main.js
@@ -6,10 +6,15 @@ let dessert = document.querySelector(".dessert");
 let drinks = document.querySelector(".drinks");
 let allBtns = document.querySelectorAll(".btn");
 let menu = document.querySelector(".menu");
+const favCount = document.querySelector(".fav-count");
 
 arr = [];
 let menus = null;
 let menusCoy = null;
+
+let favProduct = getFavaFromLocaleStrogeProduct();
+calculateCount(favProduct.length);
+let product = [];
 
 async function getData() {
   let res = await axios(`${BASE_URL}`);
@@ -17,8 +22,10 @@ async function getData() {
   menusCoy= structuredClone(menus);
   let fixed= res.data.filter((item)=> item.category == "main")
 //   console.logo(fixed);
+product = res.data;
   arr = res.data;
   drawCards(fixed);
+  
 }
 getData();
 
@@ -31,6 +38,11 @@ function drawCards(data) {
         <div class="box-body">
             <h5 class="title">${el.title}...... <a href="./details.html?id=${el.id}" >Read More</a></h5>
             <p class="desc">${el.desc}</p>
+            <i class="${
+              favProduct.some((item) => item.id === el.id)
+                ? "fa-solid fa-heart "
+                : "fa-regular fa-heart"
+            }" onclick=favIcon("${el.id}",this)></i>
         </div>
         <div class="price">
             <h3>$${el.price}</h3>
@@ -87,3 +99,38 @@ allBtns.forEach((element) => {
     drawCards(b);
     console.log(b)
   })
+
+
+  function favIcon(id, icon) {
+    if (icon.className === "fa-regular fa-heart") {
+      icon.className = "fa-solid fa-heart";
+    } else {
+      icon.className = "fa-regular fa-heart";
+    }
+  
+    let favs = getFavaFromLocaleStrogeProduct();
+  
+    let bool = favs.find((item) => item.id === id);
+    let products = product.find((item) => item.id === id);
+  
+    if (bool) {
+      favs = favs.filter((item) => item.id !== id);
+    } else {
+      favs.push(products);
+    }
+    setFavaFromLocaleStrogeProduct(favs);
+    calculateCount(favs.length);
+  }
+  
+  function setFavaFromLocaleStrogeProduct(fav) {
+    localStorage.setItem("favs", JSON.stringify(fav));
+  }
+  
+  function getFavaFromLocaleStrogeProduct() {
+    return JSON.parse(localStorage.getItem("favs")) || [];
+  }
+  
+  function calculateCount(count) {
+    favCount.textContent = count;
+  }
+  
